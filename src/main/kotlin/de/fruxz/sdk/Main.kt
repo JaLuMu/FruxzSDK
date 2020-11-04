8512fd176d3336a8caeb66d969ec867b922b304e
Fruxz <cedricspitzer@outlook.de>
2020-11-04 22:09:58 +0100

    Added basic plugin structure

---
+++ src/main/kotlin/de/fruxz/sdk/Main.kt
@@ -0,0 +1,19 @@
+package de.fruxz.sdk
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onLoad() {
+        super.onLoad()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+}
