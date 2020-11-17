9197f0fb30f0785ad0219bb511e85c6bebd898ba
Fruxz <cedricspitzer@outlook.de>
2020-11-17 22:49:04 +0100

    Added Preferences

---
+++ src/main/kotlin/de/fruxz/sdk/configuration/ActivePreference.kt
@@ -0,0 +1,63 @@
+package de.fruxz.sdk.configuration
+
+import org.jetbrains.annotations.NotNull
+
+class ActivePreference<T>(
+    val activeFile: ActiveFileController,
+    val path: String,
+    val default: T,
+    val useCache: Boolean = false
+) {
+
+    companion object {
+        val cache = HashMap<String, Any>()
+    }
+
+    fun getSuperPath() = "${activeFile.activeFile.path}::$path"
+
+    fun getContent(): T {
+        val v = cache[getSuperPath()]
+
+        if (!useCache && v != null) {
+            return try {
+                v as T
+            } catch (e: ClassCastException) {
+                setContent(default)
+                default
+            }
+        } else {
+            val e = activeFile.get<T>(path)
+
+            return if (activeFile.activeLoader.contains(path) && e != null) {
+
+                if (useCache)
+                    cache[getSuperPath()] = e
+
+                e
+            } else {
+
+                activeFile.set(path, default)
+
+                if (useCache)
+                    cache[getSuperPath()] = default!!
+
+                default
+            }
+
+        }
+
+    }
+
+    fun setContent(@NotNull value: T) {
+        try {
+
+            if (useCache)
+                cache[getSuperPath()] = value!!
+
+            activeFile.set(path = path, newValue = value)
+
+        } catch (ignore: StringIndexOutOfBoundsException) {
+        }
+    }
+
+}
