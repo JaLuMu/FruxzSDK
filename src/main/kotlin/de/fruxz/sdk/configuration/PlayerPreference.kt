3a9b31be1fdcc92b0d977423a05b8b134f964c22
Fruxz <cedricspitzer@outlook.de>
2020-11-18 09:31:01 +0100

    improved path

--- src/main/kotlin/de/fruxz/sdk/configuration/PlayerPreference.kt
+++ src/main/kotlin/de/fruxz/sdk/configuration/PlayerPreference.kt
@@ -3,4 +3,4 @@ package de.fruxz.sdk.configuration
 import org.bukkit.entity.Player
 
 class PlayerPreference<T>(player: Player, activeFile: ActiveFileController, path: String, default: T, useCache: Boolean = false) :
-    ActivePreference<T>(activeFile, "$path//${player.uniqueId}", default, useCache)
+    ActivePreference<T>(activeFile, "${player.uniqueId}.$path", default, useCache)
