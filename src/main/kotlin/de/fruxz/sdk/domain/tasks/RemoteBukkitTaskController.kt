41071e14055bdf8ba9be2597c715d927ebfa49a1
Fruxz <cedricspitzer@outlook.de>
2020-11-20 22:29:28 +0100

    Improved existing documentation

--- src/main/kotlin/de/fruxz/sdk/domain/tasks/RemoteBukkitTaskController.kt
+++ src/main/kotlin/de/fruxz/sdk/domain/tasks/RemoteBukkitTaskController.kt
@@ -7,7 +7,7 @@ interface RemoteBukkitTaskController {
     fun taskID(): Int
 
     /**
-     * May is only useful in repeat-task!
+     * May only useful in repeat-task!
      * **0 = not running; 1 = first round; 2 = ...**
      */
     var attempt: Int
