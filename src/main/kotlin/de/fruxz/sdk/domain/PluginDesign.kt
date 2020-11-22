ecbc2af4293af0fb43564a989b254498fd9999ad
Fruxz <cedricspitzer@outlook.de>
2020-11-22 16:10:47 +0100

    Added documentation
    Added documentation for PluginDesign.kt

--- src/main/kotlin/de/fruxz/sdk/domain/PluginDesign.kt
+++ src/main/kotlin/de/fruxz/sdk/domain/PluginDesign.kt
@@ -3,10 +3,7 @@ package de.fruxz.sdk.domain
 import org.bukkit.configuration.serialization.ConfigurationSerializable
 
 /**
- * @param messagePrefix The prefix of the transmissions
- * @param permissionMessage Variable: '<PERMISSION>' equals required permission
- * @param usageMessage Variable: '<USAGE>' equals command usage
- * @param clientTypeMessage Variable: '<CLIENT>' equals required type of client
+ * This class helps to easily save/write/create an unique plugin-design
  */
 class PluginDesign : ConfigurationSerializable {
     val messagePrefix: String
