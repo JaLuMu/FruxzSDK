232ccfae2b9a7661b4b78c34969293f7eed1daf4
Fruxz <cedricspitzer@outlook.de>
2020-11-22 16:33:47 +0100

    Improved PlayerPreference

--- src/main/kotlin/de/fruxz/sdk/configuration/PlayerPreference.kt
+++ src/main/kotlin/de/fruxz/sdk/configuration/PlayerPreference.kt
@@ -1,6 +1,15 @@
 package de.fruxz.sdk.configuration
 
+import de.fruxz.sdk.domain.User
 import org.bukkit.entity.Player
+import java.util.*
 
-class PlayerPreference<T>(player: Player, activeFile: ActiveFileController, path: String, default: T, useCache: Boolean = false) :
-    ActivePreference<T>(activeFile, "${player.uniqueId}.$path", default, useCache)
+class PlayerPreference<T> : ActivePreference<T> {
+
+    constructor(player: Player, activeFile: ActiveFileController, path: String, default: T, useCache: Boolean = false) : super(activeFile, "$path.${player.uniqueId}", default, useCache)
+
+    constructor(user: User, activeFile: ActiveFileController, path: String, default: T, useCache: Boolean = false) : super(activeFile, "$path.${user.player.uniqueId}", default, useCache)
+
+    constructor(id: UUID, activeFile: ActiveFileController, path: String, default: T, useCache: Boolean = false) : super(activeFile, "$path.${id}", default, useCache)
+
+}
