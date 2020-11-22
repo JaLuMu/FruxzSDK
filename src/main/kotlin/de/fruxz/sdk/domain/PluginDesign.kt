649cee04dcc4703b3c28f3dfd6343cf97f8b6c37
Fruxz <cedricspitzer@outlook.de>
2020-11-22 15:55:38 +0100

    PluginDesign is now serializable

--- src/main/kotlin/de/fruxz/sdk/domain/PluginDesign.kt
+++ src/main/kotlin/de/fruxz/sdk/domain/PluginDesign.kt
@@ -1,14 +1,42 @@
 package de.fruxz.sdk.domain
 
+import org.bukkit.configuration.serialization.ConfigurationSerializable
+
 /**
  * @param messagePrefix The prefix of the transmissions
  * @param permissionMessage Variable: '<PERMISSION>' equals required permission
  * @param usageMessage Variable: '<USAGE>' equals command usage
  * @param clientTypeMessage Variable: '<CLIENT>' equals required type of client
  */
-data class PluginDesign(
-    val messagePrefix: String,
-    val permissionMessage: String? = null,
-    val usageMessage: String? = null,
-    val clientTypeMessage: String? = null,
-)
+class PluginDesign : ConfigurationSerializable {
+    val messagePrefix: String
+    val permissionMessage: String?
+    val usageMessage: String?
+    val clientTypeMessage: String?
+
+    constructor(
+        messagePrefix: String,
+        permissionMessage: String? = null,
+        usageMessage: String? = null,
+        clientTypeMessage: String? = null
+    ) {
+        this.messagePrefix = messagePrefix
+        this.permissionMessage = permissionMessage
+        this.usageMessage = usageMessage
+        this.clientTypeMessage = clientTypeMessage
+    }
+
+    constructor(map: Map<String, Any>) {
+        messagePrefix = "${map["messagePrefix"]}"
+        permissionMessage = "${map["permissionMessage"]}"
+        usageMessage = "${map["usageMessage"]}"
+        clientTypeMessage = "${map["clientTypeMessage"]}"
+    }
+
+    override fun serialize() = mapOf(
+        "messagePrefix" to messagePrefix,
+        "permissionMessage" to permissionMessage,
+        "usageMessage" to usageMessage,
+        "clientTypeMessage" to clientTypeMessage,
+    )
+}
