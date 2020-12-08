5ff310c618b50ad6714b37ce5e2d884d804d2f9d
Fruxz <cedricspitzer@outlook.de>
2020-12-08 18:25:22 +0100

    Upgraded PlayerReceiveTransmissionActionBarEvent

--- src/main/kotlin/de/fruxz/sdk/domain/event/transmission/PlayerReceiveTransmissionActionBarEvent.kt
+++ src/main/kotlin/de/fruxz/sdk/domain/event/transmission/PlayerReceiveTransmissionActionBarEvent.kt
@@ -1,15 +1,13 @@
-package de.fruxz.sdk.domain.event
+package de.fruxz.sdk.domain.event.transmission
 
 import de.fruxz.sdk.domain.display.Transmission
 import de.fruxz.sdk.kernel.FruxzPlugin
-import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.event.Cancellable
-import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 import org.bukkit.event.player.PlayerEvent
 
-class PlayerReceiveTransmissionActionBarEvent(val plugin: FruxzPlugin, var receiver: Player, var transmission: Transmission, private var isCancelled: Boolean): PlayerEvent(receiver, false), Cancellable {
+class PlayerReceiveTransmissionActionBarEvent(override val plugin: FruxzPlugin, var receiver: Player, override var transmission: Transmission, private var isCancelled: Boolean): PlayerEvent(receiver, false), TransmissionEvent {
 
     override fun isCancelled() = isCancelled
 
