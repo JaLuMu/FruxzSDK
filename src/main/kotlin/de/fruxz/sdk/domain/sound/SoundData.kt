331bdbdcc9277e3dad765117da76c400bf1e6093
Fruxz <cedricspitzer@outlook.de>
2020-12-14 00:24:47 +0100

    Added SoundData

---
+++ src/main/kotlin/de/fruxz/sdk/domain/sound/SoundData.kt
@@ -0,0 +1,33 @@
+package de.fruxz.sdk.domain.sound
+
+import org.bukkit.Sound
+import org.bukkit.configuration.serialization.ConfigurationSerializable
+
+/**
+ * This class helps to easily manage sound and its properties
+ */
+class SoundData : ConfigurationSerializable {
+
+    var type: Sound
+    var volume: Double
+    var pitch: Double
+
+    constructor(type: Sound, volume: Double, pitch: Double) {
+        this.type = type
+        this.volume = volume
+        this.pitch = pitch
+    }
+
+    constructor(map: Map<String, Any>) {
+        type = Sound.valueOf("" + map["type"])
+        volume = (map["volume"] as Number).toDouble()
+        pitch = (map["pitch"] as Number).toDouble()
+    }
+
+    override fun serialize() = mapOf(
+        "type" to type.name,
+        "volume" to volume,
+        "pitch" to pitch,
+    )
+
+}
