f3338fe70b3319232b6960c9c6258977eebba27f
Fruxz <cedricspitzer@outlook.de>
2020-12-14 00:24:53 +0100

    Added SoundBuilder

---
+++ src/main/kotlin/de/fruxz/sdk/domain/sound/SoundBuilder.kt
@@ -0,0 +1,40 @@
+package de.fruxz.sdk.domain.sound
+
+import org.bukkit.Location
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+
+/**
+ * This class helps to easily create, manage and play minecraft-sounds
+ */
+class SoundBuilder {
+
+    private var data: SoundData
+
+    constructor(soundData: SoundData) {
+        data = soundData
+    }
+
+    constructor(type: Sound, volume: Double = 1.0, pitch: Double = 1.0) {
+        data = SoundData(type = type, volume = volume, pitch = pitch)
+    }
+
+    constructor(type: Sound, volume: Float = 1F, pitch: Float = 1F) {
+        data = SoundData(type = type, volume = volume.toDouble(), pitch = pitch.toDouble())
+    }
+
+    constructor(type: Sound, volume: Int = 1, pitch: Int = 1) {
+        data = SoundData(type = type, volume = volume.toDouble(), pitch = pitch.toDouble())
+    }
+
+    fun playSound(where: Location) {
+        where.world.playSound(where, data.type, data.volume.toFloat(), data.pitch.toFloat())
+    }
+
+    fun playSound(where: Location, vararg receivers: Player) = receivers.forEach {
+        it.playSound(where, data.type, data.volume.toFloat(), data.pitch.toFloat())
+    }
+
+    fun create() = data
+
+}
