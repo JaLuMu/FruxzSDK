3297433862c83caec167dea0544d04acafcf5ed8
Fruxz <cedricspitzer@outlook.de>
2020-12-16 13:12:29 +0100

    Added explicit documentation and wiki

--- src/main/kotlin/de/fruxz/sdk/domain/container/InventorySize.kt
+++ src/main/kotlin/de/fruxz/sdk/domain/container/InventorySize.kt
@@ -1,9 +1,18 @@
 package de.fruxz.sdk.domain.container
 
+/**
+ * This class helps to easily become one of the minecraft-support inventory sizes
+ *
+ * WIKI: [Object: InventorySize (ENUM)](https://github.com/TheFruxz/FruxzSDK/wiki/Object:-InventorySize-(ENUM))
+ */
 enum class InventorySize {
 
     LARGE, SMALL;
 
+    /**
+     * This is the value of slots, which the inventory-size [InventorySize] contains,
+     * if it is in an inventory
+     */
     val size: Int
         get() = when (this) {
 
@@ -14,6 +23,10 @@ enum class InventorySize {
 
     companion object {
 
+        /**
+         * Returns the [InventorySize] object, which can have an equal number of inventory slots ([size]).
+         * If none can be found, null is returned!
+         */
         fun sizeOf(size: Int) = when (size) {
             9*3 -> SMALL
             9*6 -> LARGE
